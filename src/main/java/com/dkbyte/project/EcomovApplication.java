9ff6f0b991baca11d943cdea387073fac8788aac
KleinKuarez <yahir.kleins@gmail.com>
2019-10-16 08:46:12 -0500

    a rama master

---
+++ src/main/java/com/dkbyte/project/EcomovApplication.java
@@ -0,0 +1,13 @@
+package com.dkbyte.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dkbyte/project/EcomovApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dkbyte.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcomovApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
