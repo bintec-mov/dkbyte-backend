dcad36292506c343f8e5cfc19978a110107e3c3e
Alejo057 <alejox123321@gmail.com>
2019-10-16 18:32:23 -0500

    kj

--- src/main/java/com/dkbyte/project/repository/PasajeroViajeRepository.java
+++ src/main/java/com/dkbyte/project/repository/PasajeroViajeRepository.java
@@ -3,10 +3,8 @@ package com.dkbyte.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD:src/main/java/com/dkbyte/project/repository/PasajeroViajeRepository.java
-public interface PasajeroViajeRepository extends JpaRepository<PasajeroViajeRepository, Long> {
-=======
 @Repository
-public interface PasajeroVehiculoRepository extends JpaRepository<PasajeroVehiculoRepository, Long> {
->>>>>>> dc4b466b03532ee3da478ef7a86507cca5e87311:src/main/java/com/dkbyte/project/repository/PasajeroVehiculoRepository.java
+public interface PasajeroViajeRepository extends JpaRepository<PasajeroViajeRepository, Long> {
 }
+
+
