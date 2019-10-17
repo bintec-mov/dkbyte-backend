0627336047b0af1638fe42aff15d1e7db83d74b3
diego <diego@MacBook-Air-de-diego.local>
2019-10-17 11:42:32 -0500

    hola

--- src/main/java/com/dkbyte/project/model/PasajeroModel.java
+++ src/main/java/com/dkbyte/project/model/PasajeroModel.java
@@ -5,13 +5,6 @@ import lombok.Data;
 @Data
 public class PasajeroModel extends PersonaModel{
     private long id;
-    private PersonaModel persona;
 
-    public PersonaModel getPersona(){
-        if(persona == null){
-            persona = new PersonaModel();
-        }
-        return persona;
-    }
 
 }
